8680d7552fd043282b8cd630af0979fea16f1f04
Valnushka <vkonson@gmail.com>
2025-09-29 23:26:31 +0300

    Add anatomy page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './component'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
