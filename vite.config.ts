c2691ad06a675f31224aeb32a833451a50ccd50c
bodynar <bodynar@gmail.com>
2025-10-10 21:59:06 +0300

    update styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
-  build: {
-    sourcemap: true,
-  },
-  base: "/rice-and-stripes/",
+	plugins: [
+		react(),
+		tsconfigPaths(),
+	],
+	resolve: {
+		alias: {
+			"@styles": "/src/shared/styles",
+		},
+	},
+	build: {
+		sourcemap: true,
+	},
+	base: "/rice-and-stripes/",
 });
