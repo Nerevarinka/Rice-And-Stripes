749fa30c04f526d4e3f6a83f9036e33a2edb9dca
Valnushka <vkonson@gmail.com>
2025-11-27 23:15:28 +0300

    fix prod build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/Rice-And-Stripes",
-  assetPrefix: "/Rice-And-Stripes/",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	output: "export",
+  	basePath: "/Rice-And-Stripes",
+  	// assetPrefix: "/Rice-And-Stripes/",
 };
 
 export default nextConfig;
