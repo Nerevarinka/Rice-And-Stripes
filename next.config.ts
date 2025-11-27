fde307017e4c584639f4f9a0f6643fa8eef4f7ac
Valnushka <vkonson@gmail.com>
2025-11-27 18:41:47 +0300

    Merge branch 'main' into dev
    # Conflicts:
    #	vite.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/Rice-And-Stripes",
+  assetPrefix: "/Rice-And-Stripes/",
 };
 
 export default nextConfig;
